711d6097150b5e14525061666f9710109fcb5619
vyshnav s <vyshnavs0003@gmail.com>
2026-03-27 19:52:06 +0530

    chore: initialize projectwith Vite + React and install dependencies

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>BudgetBasket - Project Setup Complete!</h1>
+      <p>Step 1 done. Ready for Step 2!</p>
+    </div>
+  )
+}
+
+export default App;
